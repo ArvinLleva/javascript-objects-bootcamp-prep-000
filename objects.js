cec04abf2d545fa5c4f1a4361b7f972177d04ff4
ArvinLleva <arvin.lleva@yahoo.com>
2018-05-31 21:41:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,11 @@
 var playlist = {chon: 'fluffy'}
 
 function updatePlaylist(playlist, artist, song){
-  return  
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
 }
